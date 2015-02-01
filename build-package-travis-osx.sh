171971121a2d0651fe308426879e363ca7bd1820
Joe Fitzgerald <jfitzgerald@pivotal.io>
2015-01-31 21:13:42 -0700

    Add Linting To The Build Process

--- build-package-travis-osx.sh
+++ build-package-travis-osx.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 
-echo "Downloading node v0.10.22..."
-curl -s -O http://nodejs.org/dist/v0.10.22/node-v0.10.22-darwin-x64.tar.gz
-tar -zxf node-v0.10.22-darwin-x64.tar.gz
-export PATH=$PATH:$PWD/node-v0.10.22-darwin-x64/bin
+echo "Downloading node v0.10.36..."
+curl -s -O http://nodejs.org/dist/v0.10.36/node-v0.10.36-darwin-x64.tar.gz
+tar -zxf node-v0.10.36-darwin-x64.tar.gz
+export PATH=$PATH:$PWD/node-v0.10.36-darwin-x64/bin
 
 echo "Downloading latest Atom release..."
 curl -s -L "https://atom.io/download/mac" \
-  -H 'Accept: application/octet-stream' \
-  -o atom.zip
+-H 'Accept: application/octet-stream' \
+-o atom.zip
 
 mkdir atom
 unzip -q atom.zip -d atom
@@ -21,6 +21,9 @@ atom/Atom.app/Contents/Resources/app/apm/node_modules/.bin/apm install autocompl
 
 echo "Downloading package dependencies..."
 atom/Atom.app/Contents/Resources/app/apm/node_modules/.bin/apm update
+
+echo "Linting package..."
+./node_modules/.bin/coffeelint lib spec
 
 echo "Running specs..."
 ATOM_PATH=./atom atom/Atom.app/Contents/Resources/app/apm/node_modules/.bin/apm test --path atom/Atom.app/Contents/Resources/app/atom.sh
