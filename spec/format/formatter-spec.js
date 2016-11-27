7cd970f882908a1eb817200a868334be96beb1f3
Zac Bergquist <zbergquist99@gmail.com>
2016-11-27 16:42:57 -0500

    Clean up formatter spec

--- spec/format/formatter-spec.js
+++ spec/format/formatter-spec.js
@@ -5,7 +5,9 @@ import fs from 'fs'
 import path from 'path'
 import temp from 'temp'
 
-let nl = '\n'
+const nl = '\n'
+const unformattedText = 'package main' + nl + nl + 'func main()  {' + nl + '}' + nl
+const formattedText = 'package main' + nl + nl + 'func main() {' + nl + '}' + nl
 
 describe('formatter', () => {
   let mainModule = null
@@ -13,16 +15,15 @@ describe('formatter', () => {
 
   beforeEach(() => {
     atom.config.set('go-plus.disableToolCheck', true)
-    temp.track()
-    atom.config.set('go-plus.format.formatOnSave', false)
     atom.config.set('editor.defaultLineEnding', 'LF')
+    temp.track()
 
     waitsForPromise(() => {
       return atom.packages.activatePackage('language-go')
     })
 
     runs(() => {
-      let pack = atom.packages.loadPackage('go-plus')
+      const pack = atom.packages.loadPackage('go-plus')
       pack.activateNow()
       mainModule = pack.mainModule
       mainModule.getGoconfig()
@@ -43,7 +44,7 @@ describe('formatter', () => {
     let actual
 
     beforeEach(() => {
-      let directory = fs.realpathSync(temp.mkdirSync())
+      const directory = fs.realpathSync(temp.mkdirSync())
       atom.project.setPaths([directory])
       filePath = path.join(directory, 'gofmt.go')
       fs.writeFileSync(filePath, '')
@@ -61,293 +62,85 @@ describe('formatter', () => {
       if (saveSubscription) {
         saveSubscription.dispose()
       }
-
       actual = undefined
     })
 
-    describe('when format on save is disabled and gofmt is the tool', () => {
+    describe('when format on save is disabled', () => {
       beforeEach(() => {
         atom.config.set('go-plus.format.formatOnSave', false)
-        atom.config.set('go-plus.format.tool', 'gofmt')
       })
 
-      it('does not format the file on save', () => {
-        let text = 'package main' + nl + nl + 'func main()  {' + nl + '}' + nl
-        let expected = text
-        let formatted = 'package main' + nl + nl + 'func main() {' + nl + '}' + nl
-
-        runs(() => {
-          let buffer = editor.getBuffer()
-          buffer.setText(text)
-          buffer.save()
+      describe('when gofmt is the tool', () => {
+        beforeEach(() => {
+          atom.config.set('go-plus.format.tool', 'gofmt')
         })
 
-        waitsFor(() => { return actual })
+        it('does not format the file on save', () => {
+          runs(() => {
+            const buffer = editor.getBuffer()
+            buffer.setText(unformattedText)
+            buffer.save()
+          })
 
-        runs(() => {
-          expect(actual).toBe(expected)
-          expect(actual).not.toBe(formatted)
-        })
-      })
+          waitsFor(() => { return actual })
 
-      it('formats the file on command', () => {
-        let text = 'package main' + nl + nl + 'func main()  {' + nl + '}' + nl
-        let unformatted = text
-        let formatted = 'package main' + nl + nl + 'func main() {' + nl + '}' + nl
-
-        runs(() => {
-          let buffer = editor.getBuffer()
-          buffer.setText(text)
-          buffer.save()
+          runs(() => {
+            expect(actual).toBe(unformattedText)
+            expect(actual).not.toBe(formattedText)
+          })
         })
 
-        waitsFor(() => {
-          return actual
-        })
+        it('formats the file on command', () => {
+          runs(() => {
+            const buffer = editor.getBuffer()
+            buffer.setText(unformattedText)
+            buffer.save()
+          })
 
-        runs(() => {
-          expect(actual).toBe(unformatted)
-          expect(actual).not.toBe(formatted)
-          let target = atom.views.getView(editor)
-          atom.commands.dispatch(target, 'golang:gofmt')
-        })
+          waitsFor(() => {
+            return actual
+          })
 
-        runs(() => {
-          expect(editor.getText()).toBe(formatted)
+          runs(() => {
+            expect(actual).toBe(unformattedText)
+            expect(actual).not.toBe(formattedText)
+            const target = atom.views.getView(editor)
+            atom.commands.dispatch(target, 'golang:gofmt')
+          })
+
+          runs(() => {
+            expect(editor.getText()).toBe(formattedText)
+          })
         })
       })
     })
 
-    describe('when format on save is enabled and gofmt is the tool', () => {
+    describe('when format on save is enabled', () => {
       beforeEach(() => {
         atom.config.set('go-plus.format.formatOnSave', true)
-        atom.config.set('go-plus.format.tool', 'gofmt')
       })
 
-      it('formats the file on save', () => {
-        let text = 'package main' + nl + nl + 'func main()  {' + nl + '}' + nl
-        let expected = 'package main' + nl + nl + 'func main() {' + nl + '}' + nl
+      describe('for each tool', () => {
+        for (const tool of ['gofmt', 'goimports', 'goreturns']) {
+          atom.config.set('go-plus.format.tool', tool)
 
-        runs(() => {
-          let buffer = editor.getBuffer()
-          buffer.setText(text)
-          buffer.save()
-        })
+          it('formats on save using ' + tool, () => {
+            runs(() => {
+              const buffer = editor.getBuffer()
+              buffer.setText(unformattedText)
+              buffer.save()
+            })
 
-        waitsFor(() => {
-          return actual
-        })
+            waitsFor(() => {
+              return actual
+            })
 
-        runs(() => {
-          expect(actual).toBe(expected)
-        })
-      })
-    })
-
-    describe('when format on save is enabled and goimports is the tool', () => {
-      beforeEach(() => {
-        atom.config.set('go-plus.format.formatOnSave', true)
-        atom.config.set('go-plus.format.tool', 'goimports')
-      })
-
-      it('formats the file on save', () => {
-        let text = 'package main' + nl + nl + 'func main()  {' + nl + '}' + nl
-        let expected = 'package main' + nl + nl + 'func main() {' + nl + '}' + nl
-
-        runs(() => {
-          let buffer = editor.getBuffer()
-          buffer.setText(text)
-          buffer.save()
-        })
-
-        waitsFor(() => {
-          return actual
-        })
-
-        runs(() => {
-          expect(actual).toBe(expected)
-        })
-      })
-    })
-
-    describe('when format on save is enabled and goreturns is the tool', () => {
-      beforeEach(() => {
-        atom.config.set('go-plus.format.formatOnSave', true)
-        atom.config.set('go-plus.format.tool', 'goreturns')
-      })
-
-      it('formats the file on save', () => {
-        let text = 'package main' + nl + nl + 'func main()  {' + nl + '}' + nl
-        let expected = 'package main' + nl + nl + 'func main() {' + nl + '}' + nl
-
-        runs(() => {
-          let buffer = editor.getBuffer()
-          buffer.setText(text)
-          buffer.save()
-        })
-
-        waitsFor(() => {
-          return actual
-        })
-
-        runs(() => {
-          expect(actual).toBe(expected)
-        })
+            runs(() => {
+              expect(actual).toBe(formattedText)
+            })
+          })
+        }
       })
     })
   })
 })
-
-/*
-path = require('path')
-fs = require('fs-plus')
-temp = require('temp').track()
-_ = require('lodash')
-AtomConfig = require('./util/atomconfig')
-
-describe 'format', ->
-  [mainModule, editor, dispatch, buffer, filePath] = []
-
-  beforeEach ->
-    atomconfig = new AtomConfig()
-    atomconfig.allfunctionalitydisabled()
-    directory = temp.mkdirSync()
-    atom.project.setPaths(directory)
-    filePath = path.join(directory, 'go-plus.go')
-    fs.writeFileSync(filePath, '')
-    jasmine.unspy(window, 'setTimeout')
-
-    waitsForPromise -> atom.workspace.open(filePath).then (e) ->
-      editor = e
-      buffer = editor.getBuffer()
-
-    waitsForPromise ->
-      atom.packages.activatePackage('language-go')
-
-    waitsForPromise -> atom.packages.activatePackage('go-plus').then (g) ->
-      mainModule = g.mainModule
-
-    waitsFor ->
-      mainModule.dispatch?.ready
-
-    runs ->
-      dispatch = mainModule.dispatch
-
-  describe 'when format on save is enabled', ->
-    beforeEach ->
-      atom.config.set('go-plus.format.formatOnSave', true)
-
-    it 'reformats the file', ->
-      done = false
-      runs ->
-        dispatch.once 'dispatch-complete', ->
-          expect(fs.readFileSync(filePath, {encoding: 'utf8'})).toBe('package main\n\nfunc main() {\n}\n')
-          expect(dispatch.messages?).toBe(true)
-          expect(_.size(dispatch.messages)).toBe(0)
-          done = true
-        buffer.setText('package main\n\nfunc main()  {\n}\n')
-        buffer.save()
-
-      waitsFor ->
-        done is true
-
-    it 'reformats the file after multiple saves', ->
-      done = false
-      displayDone = false
-
-      runs ->
-        dispatch.once 'dispatch-complete', ->
-          expect(fs.readFileSync(filePath, {encoding: 'utf8'})).toBe('package main\n\nfunc main() {\n}\n')
-          expect(dispatch.messages?).toBe(true)
-          expect(_.size(dispatch.messages)).toBe(0)
-          done = true
-        dispatch.once 'display-complete', ->
-          displayDone = true
-        buffer.setText('package main\n\nfunc main()  {\n}\n')
-        buffer.save()
-
-      waitsFor ->
-        done is true
-
-      waitsFor ->
-        displayDone is true
-
-      runs ->
-        done = false
-        dispatch.once 'dispatch-complete', ->
-          expect(fs.readFileSync(filePath, {encoding: 'utf8'})).toBe('package main\n\nfunc main() {\n}\n')
-          expect(dispatch.messages?).toBe(true)
-          expect(_.size(dispatch.messages)).toBe(0)
-          done = true
-        buffer.setText('package main\n\nfunc main()  {\n}\n')
-        buffer.save()
-
-      waitsFor ->
-        done is true
-
-    it 'collects errors when the input is invalid', ->
-      done = false
-      runs ->
-        dispatch.once 'dispatch-complete', (editor) ->
-          expect(fs.readFileSync(filePath, {encoding: 'utf8'})).toBe('package main\n\nfunc main(!)  {\n}\n')
-          expect(dispatch.messages?).toBe(true)
-          expect(_.size(dispatch.messages)).toBe(1)
-          expect(dispatch.messages[0].column).toBe('11')
-          expect(dispatch.messages[0].line).toBe('3')
-          expect(dispatch.messages[0].msg).toBe('expected type, found \'!\'')
-          done = true
-        buffer.setText('package main\n\nfunc main(!)  {\n}\n')
-        buffer.save()
-
-      waitsFor ->
-        done is true
-
-    it 'uses goimports to reorganize imports if enabled', ->
-      done = false
-      runs ->
-        atom.config.set('go-plus.format.tool', 'goimports')
-        dispatch.once 'dispatch-complete', ->
-          expect(fs.readFileSync(filePath, {encoding: 'utf8'})).toBe('package main\n\nimport "fmt"\n\nfunc main() {\n\tfmt.Println("Hello, 世界")\n}\n')
-          expect(dispatch.messages?).toBe(true)
-          expect(_.size(dispatch.messages)).toBe(0)
-          done = true
-        buffer.setText('package main\n\nfunc main()  {\n\tfmt.Println("Hello, 世界")\n}\n')
-        buffer.save()
-
-      waitsFor ->
-        done is true
-
-    it 'uses goreturns to handle returns if enabled', ->
-      done = false
-      runs ->
-        atom.config.set('go-plus.format.tool', 'goreturns')
-        dispatch.once 'dispatch-complete', ->
-          expect(fs.readFileSync(filePath, {encoding: 'utf8'})).toBe('package demo\n\nimport "errors"\n\nfunc F() (string, int, error) {\n\treturn "", 0, errors.New("foo")\n}\n')
-          expect(dispatch.messages?).toBe(true)
-          expect(_.size(dispatch.messages)).toBe(0)
-          done = true
-        buffer.setText('package demo\n\nfunc F() (string, int, error)     {\nreturn errors.New("foo") }')
-        buffer.save()
-
-      waitsFor ->
-        done is true
-
-  describe 'when format on save is disabled', ->
-    beforeEach ->
-      atom.config.set('go-plus.format.formatOnSave', false)
-
-    it 'does not reformat the file', ->
-      done = false
-      runs ->
-        dispatch.once 'dispatch-complete', ->
-          expect(fs.readFileSync(filePath, {encoding: 'utf8'})).toBe('package main\n\nfunc main()  {\n}\n')
-          expect(dispatch.messages?).toBe(true)
-          expect(_.size(dispatch.messages)).toBe(0)
-          done = true
-        buffer.setText('package main\n\nfunc main()  {\n}\n')
-        buffer.save()
-
-      waitsFor ->
-        done is true
-
-*/
