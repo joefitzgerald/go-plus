d7568709cd47f83110847f8add8daa411581fefe
Zac Bergquist <zbergquist99@gmail.com>
2017-02-25 11:34:53 -0700

    Add support for did save events

--- spec/save-event-spec.js
+++ spec/save-event-spec.js
@@ -15,6 +15,91 @@ describe('save event orchestrator', () => {
     saveEvent.dispose()
   })
 
+  describe('onDidSave subscription', () => {
+    it('throws an error if a callback is not provided', () => {
+      expect(() => { saveEvent.onDidSave() }).toThrow()
+    })
+
+    it('throws an error if the callback is not a function', () => {
+      expect(() => { saveEvent.onDidSave(42) }).toThrow()
+    })
+
+    it('registers a callback that can be unregistered', () => {
+      const callback = () => {}
+      const disp = saveEvent.onDidSave(callback)
+      expect(saveEvent.didSaveCallbacks.size).toBe(1)
+
+      disp.dispose()
+      expect(saveEvent.didSaveCallbacks.size).toBe(0)
+    })
+
+    it('runs a single callback', () => {
+      let called = false
+
+      runs(() => {
+        const callback = () => { called = true; return true }
+        saveEvent.onDidSave(callback)
+      })
+
+      waitsForPromise(() => {
+        const filepath = path.join(__dirname, 'fixtures', 'main.go')
+        return atom.workspace.open(filepath).then((e) => {
+          e.save()
+        })
+      })
+
+      runs(() => {
+        expect(called).toBe(true)
+      })
+    })
+
+    it('runs multiple callbacks', () => {
+      let called = [false, false]
+
+      runs(() => {
+        const callback0 = () => { called[0] = true; return true }
+        const callback1 = () => { called[1] = true; return true }
+        saveEvent.onDidSave(callback0)
+        saveEvent.onDidSave(callback1)
+      })
+
+      waitsForPromise(() => {
+        const filepath = path.join(__dirname, 'fixtures', 'main.go')
+        return atom.workspace.open(filepath).then((e) => {
+          e.save()
+        })
+      })
+
+      runs(() => {
+        expect(called[0]).toBe(true)
+        expect(called[1]).toBe(true)
+      })
+    })
+
+    it('stops invoking callbacks when encountering a non-true return value', () => {
+      let called = [false, false]
+
+      runs(() => {
+        const callback0 = () => { called[0] = true; return false }
+        const callback1 = () => { called[1] = true; return true }
+        saveEvent.onDidSave(callback0)
+        saveEvent.onDidSave(callback1)
+      })
+
+      waitsForPromise(() => {
+        const filepath = path.join(__dirname, 'fixtures', 'main.go')
+        return atom.workspace.open(filepath).then((e) => {
+          e.save()
+        })
+      })
+
+      runs(() => {
+        expect(called[0]).toBe(true)
+        expect(called[1]).toBe(false)
+      })
+    })
+  })
+
   describe('onWillSave subscription', () => {
     it('throws an error if a callback is not provided', () => {
       expect(() => { saveEvent.onWillSave() }).toThrow()
