a5f193e08297205805fab4e33aa2ec5b5d6d0f46
Zac Bergquist <zbergquist99@gmail.com>
2017-02-25 11:25:36 -0700

    Add save event orchestrator
    This will be a one stop shop for subscribing to on{Will|Did}Save events.
    
    Currently we only support `onWillSave` events.  Support for `onDidSave` is forthcoming.
    
    The orchestrator executes callbacks in the order they are registered,
    and allows a callback to prevent execution of future callbacks.
    
    Convert formatter over to using the orchestrator rather than subscribing manually.

---
+++ spec/save-event-spec.js
@@ -0,0 +1,102 @@
+'use babel'
+/* eslint-env jasmine */
+
+import path from 'path'
+import SaveEventOrchestrator from './../lib/save-event'
+
+describe('save event orchestrator', () => {
+  let saveEvent = null
+
+  beforeEach(() => {
+    saveEvent = new SaveEventOrchestrator()
+  })
+
+  afterEach(() => {
+    saveEvent.dispose()
+  })
+
+  describe('onWillSave subscription', () => {
+    it('throws an error if a callback is not provided', () => {
+      expect(() => { saveEvent.onWillSave() }).toThrow()
+    })
+
+    it('throws an error if the callback is not a function', () => {
+      expect(() => { saveEvent.onWillSave(42) }).toThrow()
+    })
+
+    it('registers a callback that can be unregistered', () => {
+      const callback = () => {}
+      const disp = saveEvent.onWillSave(callback)
+      expect(saveEvent.willSaveCallbacks.size).toBe(1)
+
+      disp.dispose()
+      expect(saveEvent.willSaveCallbacks.size).toBe(0)
+    })
+
+    it('runs a single callback', () => {
+      let called = false
+
+      runs(() => {
+        const callback = () => { called = true; return true }
+        saveEvent.onWillSave(callback)
+      })
+
+      waitsForPromise(() => {
+        const filepath = path.join(__dirname, 'fixtures', 'main.go')
+        return atom.workspace.open(filepath).then((e) => {
+          e.save()
+        })
+      })
+
+      runs(() => {
+        expect(called).toBe(true)
+      })
+    })
+
+    it('runs multiple callbacks', () => {
+      let called = [false, false]
+
+      runs(() => {
+        const callback0 = () => { called[0] = true; return true }
+        const callback1 = () => { called[1] = true; return true }
+        saveEvent.onWillSave(callback0)
+        saveEvent.onWillSave(callback1)
+      })
+
+      waitsForPromise(() => {
+        const filepath = path.join(__dirname, 'fixtures', 'main.go')
+        return atom.workspace.open(filepath).then((e) => {
+          e.save()
+        })
+      })
+
+      runs(() => {
+        expect(called[0]).toBe(true)
+        expect(called[1]).toBe(true)
+      })
+    })
+
+    it('stops invoking callbacks when encountering a non-true return value', () => {
+      let called = [false, false]
+
+      runs(() => {
+        const callback0 = () => { called[0] = true; return false }
+        const callback1 = () => { called[1] = true; return true }
+        saveEvent.onWillSave(callback0)
+        saveEvent.onWillSave(callback1)
+      })
+
+      waitsForPromise(() => {
+        const filepath = path.join(__dirname, 'fixtures', 'main.go')
+        return atom.workspace.open(filepath).then((e) => {
+          e.save()
+        })
+      })
+
+      runs(() => {
+        expect(called[0]).toBe(true)
+        expect(called[1]).toBe(false)
+      })
+    })
+  })
+})
