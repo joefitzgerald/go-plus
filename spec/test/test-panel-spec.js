9050354cbc02b99017282fc732482230ff21f0f6
Joe Fitzgerald <jfitzgerald@pivotal.io>
2017-04-01 14:55:30 -0600

    Fix specs, upgrade etch-octicon

--- spec/test/test-panel-spec.js
+++ spec/test/test-panel-spec.js
@@ -18,10 +18,10 @@ describe('test panel', () => {
 
     it('renders a link', () => {
       const elements = testPanel.makeLink('/Users/user/go/src/foo/bar.go:23')
-      console.log(elements)
+      expect(elements).toBeTruthy()
       expect(elements.length).toBe(2)
-      expect(elements[0].tagName).toBe('SPAN')
-      expect(elements[1].tagName).toBe('A')
+      expect(elements[0].tag).toBe('span')
+      expect(elements[1].tag).toBe('a')
       expect(elements[0].children[0].text).toBe('')
       expect(elements[1].children[0].text).toBe('/Users/user/go/src/foo/bar.go:23')
     })
@@ -29,50 +29,50 @@ describe('test panel', () => {
     it('renders a link to a go file (relative path)', () => {
       const elements = testPanel.makeLink('failure at foo/bar.go:23')
       expect(elements.length).toBe(2)
-      expect(elements[0].tagName).toBe('SPAN')
-      expect(elements[1].tagName).toBe('A')
+      expect(elements[0].tag).toBe('span')
+      expect(elements[1].tag).toBe('a')
       expect(elements[1].children[0].text).toBe('foo/bar.go:23')
     })
 
     it('renders a link to a go file (absolute path)', () => {
       const elements = testPanel.makeLink('failure at /home/user/go/src/foo/bar.go:23')
       expect(elements.length).toBe(2)
-      expect(elements[0].tagName).toBe('SPAN')
-      expect(elements[1].tagName).toBe('A')
+      expect(elements[0].tag).toBe('span')
+      expect(elements[1].tag).toBe('a')
       expect(elements[1].children[0].text).toBe('/home/user/go/src/foo/bar.go:23')
     })
 
     it('renders a link to a test go file (relative path)', () => {
       const elements = testPanel.makeLink('failure at foo/bar_test.go:23')
       expect(elements.length).toBe(2)
-      expect(elements[0].tagName).toBe('SPAN')
-      expect(elements[1].tagName).toBe('A')
+      expect(elements[0].tag).toBe('span')
+      expect(elements[1].tag).toBe('a')
       expect(elements[1].children[0].text).toBe('foo/bar_test.go:23')
     })
 
     it('renders a link to a test go file (absolute path)', () => {
       const elements = testPanel.makeLink('failure at /home/user/go/src/foo/bar_test.go:23')
       expect(elements.length).toBe(2)
-      expect(elements[0].tagName).toBe('SPAN')
-      expect(elements[1].tagName).toBe('A')
+      expect(elements[0].tag).toBe('span')
+      expect(elements[1].tag).toBe('a')
       expect(elements[1].children[0].text).toBe('/home/user/go/src/foo/bar_test.go:23')
     })
 
     it('renders links to Windows paths', () => {
       const elements = testPanel.makeLink('failure at C:\\Users\\Me\\go\\src\\foo\\bar_test.go:23')
       expect(elements.length).toBe(2)
-      expect(elements[0].tagName).toBe('SPAN')
-      expect(elements[1].tagName).toBe('A')
+      expect(elements[0].tag).toBe('span')
+      expect(elements[1].tag).toBe('a')
       expect(elements[1].children[0].text).toBe('C:\\Users\\Me\\go\\src\\foo\\bar_test.go:23')
     })
 
     it('renders multiple links', () => {
       const elements = testPanel.makeLink('failures at foo/bar.go:12 and baz/quux.go:34')
       expect(elements.length).toBe(4)
-      expect(elements[0].tagName).toBe('SPAN')
-      expect(elements[1].tagName).toBe('A')
-      expect(elements[2].tagName).toBe('SPAN')
-      expect(elements[3].tagName).toBe('A')
+      expect(elements[0].tag).toBe('span')
+      expect(elements[1].tag).toBe('a')
+      expect(elements[2].tag).toBe('span')
+      expect(elements[3].tag).toBe('a')
 
       expect(elements[1].children[0].text).toBe('foo/bar.go:12')
       expect(elements[3].children[0].text).toBe('baz/quux.go:34')
@@ -81,9 +81,9 @@ describe('test panel', () => {
     it('renders a link with prefix and suffix', () => {
       const elements = testPanel.makeLink('prefix foo/bar.go:23 suffix')
       expect(elements.length).toBe(3)
-      expect(elements[0].tagName).toBe('SPAN')
-      expect(elements[1].tagName).toBe('A')
-      expect(elements[2].tagName).toBe('SPAN')
+      expect(elements[0].tag).toBe('span')
+      expect(elements[1].tag).toBe('a')
+      expect(elements[2].tag).toBe('span')
 
       expect(elements[0].children[0].text).toBe('prefix ')
       expect(elements[1].children[0].text).toBe('foo/bar.go:23')
