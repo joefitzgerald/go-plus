f9ee577249a12ab6af27414eedd78e1d4115b5be
Joe Fitzgerald <jfitzgerald@pivotal.io>
2017-05-13 10:38:53 -0600

    Remove log statement

--- spec/build/builder-spec.js
+++ spec/build/builder-spec.js
@@ -96,7 +96,6 @@ describe('builder', () => {
 
       let message = messages[0]
       expect(message.name).toEqual('build')
-      console.log(message.excerpt)
       expect(message.excerpt.indexOf('syntax error: unexpected semicolon or newline, expecting comma or }') === 0).toBeTruthy()
       expect(message.location.file.indexOf('the-file.go') > 0).toBeTruthy() // file is in the path
       expect(message.location.file.indexOf('sample-project') > 0).toBeTruthy() // cwd is in the path
