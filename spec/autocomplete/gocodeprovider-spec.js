a0de53e8c2e16def874211bb0af9d174397811f9
Joe Fitzgerald <jfitzgerald@pivotal.io>
2018-01-26 15:22:53 -0700

    Re-introduce caching, and clear the cache appropriately

--- spec/autocomplete/gocodeprovider-spec.js
+++ spec/autocomplete/gocodeprovider-spec.js
@@ -210,7 +210,7 @@ describe('gocodeprovider', () => {
         atom.config.set('go-plus.autocomplete.snippetMode', 'nameAndType')
       })
 
-      it('provides the exported types of the unimported package', () => {
+      fit('provides the exported types of the unimported package', () => {
         let suggestions = null
 
         waitsFor(() => provider.allPkgs.size > 0)
