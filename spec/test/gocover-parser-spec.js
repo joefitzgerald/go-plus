0cee9ec4dc21efe439a051244ed76a2d629ed968
Zac Bergquist <zbergquist99@gmail.com>
2018-11-10 09:43:05 -0700

    Clean up parser spec

--- spec/test/gocover-parser-spec.js
+++ spec/test/gocover-parser-spec.js
@@ -1,4 +1,3 @@
-'use babel'
 /* eslint-env jasmine */
 
 import { ranges } from './../../lib/test/gocover-parser'
@@ -14,122 +13,95 @@ describe('gocover-parser', () => {
   let filePath
   let testFilePath
 
-  beforeEach(() => {
-    runs(() => {
-      lifecycle.setup()
-    })
+  beforeEach(async () => {
+    lifecycle.setup()
+    await lifecycle.activatePackage()
+    goconfig = lifecycle.mainModule.provideGoConfig()
 
-    waitsForPromise(() => {
-      return lifecycle.activatePackage()
-    })
-
-    runs(() => {
-      goconfig = lifecycle.mainModule.provideGoConfig()
-    })
-
-    runs(() => {
-      directory = lifecycle.temp.mkdirSync()
-      env = Object.assign({}, goconfig.environment())
-      env['GOPATH'] = directory
-      filePath = path.join(
-        directory,
-        'src',
-        'github.com',
-        'testuser',
-        'example',
-        'go-plus.go'
-      )
-      testFilePath = path.join(
-        directory,
-        'src',
-        'github.com',
-        'testuser',
-        'example',
-        'go-plus_test.go'
-      )
-      fs.ensureDirSync(path.dirname(filePath))
-      fs.ensureDirSync(path.dirname(testFilePath))
-      fs.writeFileSync(
-        filePath,
-        'package main\n\nimport "fmt"\n\nfunc main()  {\n\tfmt.Println(Hello())\n}\n\nfunc Hello() string {\n\treturn "Hello, 世界"\n}\n'
-      )
-      fs.writeFileSync(
-        testFilePath,
-        'package main\n\nimport "testing"\n\nfunc TestHello(t *testing.T) {\n\tresult := Hello()\n\tif result != "Hello, 世界" {\n\t\tt.Errorf("Expected %s - got %s", "Hello, 世界", result)\n\t}\n}'
-      )
-    })
+    directory = lifecycle.temp.mkdirSync()
+    env = Object.assign({}, goconfig.environment())
+    env['GOPATH'] = directory
+    filePath = path.join(
+      directory,
+      'src',
+      'github.com',
+      'testuser',
+      'example',
+      'go-plus.go'
+    )
+    testFilePath = path.join(
+      directory,
+      'src',
+      'github.com',
+      'testuser',
+      'example',
+      'go-plus_test.go'
+    )
+    fs.ensureDirSync(path.dirname(filePath))
+    fs.ensureDirSync(path.dirname(testFilePath))
+    fs.writeFileSync(
+      filePath,
+      'package main\n\nimport "fmt"\n\nfunc main()  {\n\tfmt.Println(Hello())\n}\n\nfunc Hello() string {\n\treturn "Hello, 世界"\n}\n'
+    )
+    fs.writeFileSync(
+      testFilePath,
+      'package main\n\nimport "testing"\n\nfunc TestHello(t *testing.T) {\n\tresult := Hello()\n\tif result != "Hello, 世界" {\n\t\tt.Errorf("Expected %s - got %s", "Hello, 世界", result)\n\t}\n}'
+    )
   })
 
   afterEach(() => {
     lifecycle.teardown()
   })
 
-  it('parses the file for a single package', () => {
+  it('parses the file for a single package', async () => {
     let cmd
     let tempDir = lifecycle.temp.mkdirSync()
     let tempFile = path.join(tempDir, 'coverage.out')
     let args = ['test', '-coverprofile=' + tempFile]
     let cwd = path.join(directory, 'src', 'github.com', 'testuser', 'example')
-    let p = goconfig.locator.findTool('go').then(c => {
-      expect(c).toBeTruthy()
-      cmd = c
-      return
-    })
+    cmd = await goconfig.locator.findTool('go')
+    expect(cmd).toBeTruthy()
 
-    waitsForPromise(() => {
-      return p
-    })
+    let executorOptions = { cwd: cwd, env: env }
+    const execResult = await goconfig.executor.exec(cmd, args, executorOptions)
+    expect(execResult.exitcode).toBe(0)
+    expect(execResult.stderr).toBeFalsy()
+    expect(execResult.stdout).toBeTruthy()
 
-    runs(() => {
-      let executorOptions = { cwd: cwd, env: env }
-      p = goconfig.executor.exec(cmd, args, executorOptions).then(r => {
-        expect(r.exitcode).toBe(0)
-        expect(r.stderr).toBeFalsy()
-        expect(r.stdout).toBeTruthy()
-        return
-      })
-    })
+    let retext = '^' + path.join(directory, 'src') + path.sep
+    if (os.platform() === 'win32') {
+      retext = retext.replace(/\\/g, '\\\\')
+    }
 
-    waitsForPromise(() => {
-      return p
-    })
+    let re = new RegExp(retext)
+    let packagePath = filePath.replace(re, '')
 
-    runs(() => {
-      let retext = '^' + path.join(directory, 'src') + path.sep
-      if (os.platform() === 'win32') {
-        retext = retext.replace(/\\/g, '\\\\')
-      }
+    let r = ranges(tempFile)
+    expect(r).toBeTruthy()
+    expect(r.length).toBeGreaterThan(0)
+    const result = r.filter(item => filePath.endsWith(item.file))
+    console.log('ranges', r) // eslint-disable-line no-console
+    console.log('filtered for ' + filePath, result) // eslint-disable-line no-console
+    expect(result).toBeTruthy()
+    expect(result.length).toBe(2)
+    expect(result[0]).toBeDefined()
+    expect(result[0].range.start).toBeDefined()
+    expect(result[0].range.end).toBeDefined()
+    expect(result[0].range.start.column).toBe(13)
+    expect(result[0].range.start.row).toBe(4)
+    expect(result[0].range.end.column).toBe(1)
+    expect(result[0].range.end.row).toBe(6)
+    expect(result[0].count).toBe(0)
+    expect(result[0].file).toBe(packagePath)
 
-      let re = new RegExp(retext)
-      let packagePath = filePath.replace(re, '')
-
-      let r = ranges(tempFile)
-      expect(r).toBeTruthy()
-      expect(r.length).toBeGreaterThan(0)
-      const result = r.filter(item => filePath.endsWith(item.file))
-      console.log('ranges', r) // eslint-disable-line no-console
-      console.log('filtered for ' + filePath, result) // eslint-disable-line no-console
-      expect(result).toBeTruthy()
-      expect(result.length).toBe(2)
-      expect(result[0]).toBeDefined()
-      expect(result[0].range.start).toBeDefined()
-      expect(result[0].range.end).toBeDefined()
-      expect(result[0].range.start.column).toBe(13)
-      expect(result[0].range.start.row).toBe(4)
-      expect(result[0].range.end.column).toBe(1)
-      expect(result[0].range.end.row).toBe(6)
-      expect(result[0].count).toBe(0)
-      expect(result[0].file).toBe(packagePath)
-
-      expect(result[1]).toBeDefined()
-      expect(result[1].range.start).toBeDefined()
-      expect(result[1].range.end).toBeDefined()
-      expect(result[1].range.start.column).toBe(20)
-      expect(result[1].range.start.row).toBe(8)
-      expect(result[1].range.end.column).toBe(1)
-      expect(result[1].range.end.row).toBe(10)
-      expect(result[1].count).toBe(1)
-      expect(result[1].file).toBe(packagePath)
-    })
+    expect(result[1]).toBeDefined()
+    expect(result[1].range.start).toBeDefined()
+    expect(result[1].range.end).toBeDefined()
+    expect(result[1].range.start.column).toBe(20)
+    expect(result[1].range.start.row).toBe(8)
+    expect(result[1].range.end.column).toBe(1)
+    expect(result[1].range.end.row).toBe(10)
+    expect(result[1].count).toBe(1)
+    expect(result[1].file).toBe(packagePath)
   })
 })
